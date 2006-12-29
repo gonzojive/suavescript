72ffdef1beec453e74830c15ad449ca308a52d8f
reddaly <reddaly@ced835d6-7b1c-0410-8bf7-d5b9059ba6eb>
2006-12-29 23:54:48 +0000

    Adding xml-mop, a library for working with XML elements as CLOS classes, similar to XMLisp without a focus (yet) on writing XML output.

---
+++ xml-mop/src/package.lisp
@@ -0,0 +1,7 @@
+(defpackage org.iodb.xml-mop
+  (:nicknames xml-mop)
+  (:documentation "xml-mop allows representing parts of XML documents as CLOS objects")
+  (:use :cl :s-xml :closer-mop))
+(in-package :xml-mop)
+
+
