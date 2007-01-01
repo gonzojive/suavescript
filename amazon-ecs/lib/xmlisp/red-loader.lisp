9a7b12a80231afed4126e7e21d8cbde8b6d7207d
reddaly <reddaly@ced835d6-7b1c-0410-8bf7-d5b9059ba6eb>
2007-01-01 01:51:50 +0000

    switching to xml-mop for amazon-ecs model

--- amazon-ecs/lib/xmlisp/red-loader.lisp
+++
@@ -1,5 +0,0 @@
-(dolist (fname '("package.lisp" "xml-utilities.lisp" "element-definition.lisp"
-		 "xml-tag-definitions.lisp" "element-implementation.lisp"
-		 "name-character-check.lisp" "xml-parser.lisp" "xml-writer.lisp"
-		 "XMLisp.lisp"))
-  (load (format nil "/home/red/code/lib/xmlisp/~A" fname)))
